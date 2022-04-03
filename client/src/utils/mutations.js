659c9288f661c722f715e0606be9c9115d2b7ebc
Scott Rohrig <scott.rohrig@gmail.com>
2022-04-02 21:15:22 -0700

    fix addUser bug not running properly

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -18,12 +18,14 @@ export const LOGIN_USER = gql`
 // mutation for 'signup' named addUser takes in username, email, password
 // returns token & user
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser( username: $username , email: $email , password: $password )
-    token
-    user {
-      _id
-      username
+  mutation addUser($username: String!,$password: String!, $email: String! ) {
+    addUser( username: $username , password: $password, email: $email ) {
+      token
+      user {
+        _id
+        username
+        email
+      }
     }
   }
 `;
